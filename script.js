af6a1e05fa8b541ceef1271717b39da76363cc6f
Jose Ruiz <38330657+jruiiz@users.noreply.github.com>
2024-12-20 12:00:41 -0500

    feat: Add human function and readChoice function

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 let getComputerChoice = () => {
     // Get random number
-    let randomNum = Math.floor(Math.random() * 3);
+    let computerChoice = Math.floor(Math.random() * 3);
+    console.log("Computer choice: " + readChoice(computerChoice));
+};
 
+let getHumanChoice = () => {
+    // Get user input through browser window.
+    // Convert string to int to later pass into choice reader
+    let humanChoice = parseInt(prompt("Enter number to choose rock [0], paper [1], or scissors [2]"));
+    console.log("Human choice: " + readChoice(humanChoice));
+};
+
+let readChoice = (choice) => {
     // Conditional checks
-    if (!randomNum) { // Return rock on 0
+    if (!choice) { // Return rock on 0
         return "rock";
-    } else if (randomNum === 1) { // Return paper on 1
+    } else if (choice === 1) { // Return paper on 1
         return "paper";
     } else { // Return scissors on 2 (otherwise since 0 <= x <= 2)
         return "scissors";
     }
-};
+}
+
+getComputerChoice();
+getHumanChoice();
