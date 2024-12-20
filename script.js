b99d1cd4f9d2c40617d715573e3b4fbf936ef470
Jose Ruiz <38330657+jruiiz@users.noreply.github.com>
2024-12-20 12:09:59 -0500

    feat: Update to meet TOP specifications

--- script.js
+++ script.js
@@ -1,26 +1,41 @@
+let humanScore = 0;
+let computerScore = 0;
+
 let getComputerChoice = () => {
     // Get random number
     let computerChoice = Math.floor(Math.random() * 3);
-    console.log("Computer choice: " + readChoice(computerChoice));
+
+    let resultStr;
+
+    // Conditional checks
+    if (!choice) { // Return rock on 0
+        resultStr = "rock";
+    } else if (choice === 1) { // Return paper on 1
+        resultStr = "paper";
+    } else { // Return scissors on 2 (otherwise since 0 <= x <= 2)
+        resultStr = "scissors";
+    }
+
+    console.log("Computer choice: " + resultStr);
 };
 
 let getHumanChoice = () => {
     // Get user input through browser window.
     // Convert string to int to later pass into choice reader
-    let humanChoice = parseInt(prompt("Enter number to choose rock [0], paper [1], or scissors [2]"));
-    console.log("Human choice: " + readChoice(humanChoice));
+    let humanChoice = prompt("Enter rock, paper, or scissors");
+
+    // Need to make user input lowercase for comparisons later
+    let choiceLowercase = humanChoice.toLowerCase();
+    console.log("Human choice: " + choiceLowercase);
+
+    return choiceLowercase;
 };
 
-let readChoice = (choice) => {
-    // Conditional checks
-    if (!choice) { // Return rock on 0
-        return "rock";
-    } else if (choice === 1) { // Return paper on 1
-        return "paper";
-    } else { // Return scissors on 2 (otherwise since 0 <= x <= 2)
-        return "scissors";
-    }
-}
 
-getComputerChoice();
-getHumanChoice();
+let playRound = (humanChoice, computerChoice) => {
+    if (humanChoice === computerChoice) { // Draw
+
+    }
+};
+
+playRound(getHumanChoice(), getComputerChoice());
